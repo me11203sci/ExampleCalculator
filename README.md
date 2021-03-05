87f95ba2621c33f4b8ec47679c29cd30a0626736
me11203sci <melesio.albavera@acecharterhigh.org>
2021-03-04 22:56:39 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+Let it be noted I started this repository on __March 3rd, 2021 at 1:00 P.M. Pacific Standard Time__. That is all I have to say. That and I have a problem.
+
+__- Melesio Albavera__
+
+___Post Script:__ Commited code for first time on __March 4th, 2021 at 10:57 P.M. Pacific Standard Time__._
---
+++ src/Main.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.regex.Pattern;
+
+public class Main
+{
+	private static boolean validParentheses(String string)
+	{
+		Stack<Character> stack = new Stack<>();
+		for(char token: string.replaceAll("[\\d\\s\\-+*/.^]", "").toCharArray())
+		{
+			if (token == '(')  stack.push(token);
+			else
+			{
+				if (stack.empty()) return false;
+				stack.pop();
+			}
+		}
+		return stack.empty();
+	}
+
+	private static boolean verify(String input)
+	{
+		return validParentheses(input) && !input.matches("(.*[^\\d\\s\\-+*/().^].*)|(.*^[.+*/^)].*)|(.*[.\\-+*/(^]$.*)|(.*[.+*/^]{2,}.*)|(.*[(-][.+*/^)].*)|(.*\\d+\\(.*)|(.*/0.*)|(.*0+[\\.1-9].*)");
+	}
+
+	private static String[] tokenize(String string)
+	{
+		return Pattern.compile("((?<=^|[+\\-/^*])-?[\\d.]+)|([+\\-/^*)(])|([\\d.]+)", Pattern.MULTILINE).matcher(string).replaceAll("$0 ").split("\\s");
+	}
+
+	private static String[] reversePolish(String[] tokenStream)
+	{
+		Stack<String> operators = new Stack<>();
+		ArrayList<String> result = new ArrayList<>();
+		Map<String, Integer> map = Map.of("(", 0, "+", 1, "-", 1, "*", 2, "/", 2, "^", 3);
+		for(String token: tokenStream)
+		{
+			if(token.matches(".*\\d.*")) result.add(token);
+			else if(token.equals("(")) operators.push(token);
+			else if (token.equals(")"))
+			{
+				while(!operators.empty() && !operators.peek().matches("\\(")) result.add(operators.pop());
+				operators.pop();
+			}
+			else
+			{
+				while(!operators.empty() && map.get(token) <= map.get(operators.peek())) result.add(operators.pop());
+				operators.push(token);
+			}
+		}
+		while(!operators.empty()) result.add(operators.pop());
+		return result.toArray(String[]::new);
+	}
+
+	private static double calculate(double a, double b, String c)
+	{
+		return switch (c)
+		{
+			case "+" -> b + a;
+			case "-" -> b - a;
+			case "*" -> b * a;
+			case "/" -> b / a;
+			default -> Math.pow(b, a);
+		};
+	}
+
+	private static String evaluate(String input)
+	{
+		Stack<String> result = new Stack<>();
+		for(String token: reversePolish(tokenize(input)))
+		{
+			if(token.matches(".*\\d.*")) result.push(token);
+			else result.push(String.valueOf(calculate(Double.parseDouble(result.pop()), Double.parseDouble(result.pop()), token)));
+		}
+		return result.pop();
+	}
+
+	public static void main(String[] args)
+	{
+		final Scanner console = new Scanner(System.in);
+		System.out.print("Enter the expression to be evaluated:\n>> ");
+		while(true)
+		{
+			String userInput;
+			System.out.print((verify((userInput = console.nextLine().replaceAll(" ", ""))))? "= " + evaluate(userInput) + "\n>> ": "Invalid expression. Try again.\n>> ");
+		}
+	}
+}
